e3742bf6fdb3fa83e00065896c01c0271438105d
Denis Pitsul <denia.pitsul@gmail.com>
2025-04-22 13:57:38 +0300

    fix variables naming

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,15 +4,13 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const COST_FOR_DAY = 40;
-
+  const PRICE_FOR_DAY = 40;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
-
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
 
-  const basePrice = COST_FOR_DAY * days;
+  const basePrice = PRICE_FOR_DAY * days;
 
   if (days >= LONG_TERM) {
     return basePrice - LONG_TERM_DISCOUNT;
